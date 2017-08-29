b347125961e5e48eb34d2eb9afb8c918b481b95c
uchihasatoshi <uchihasatoshi@163.com>
2017-08-29 23:17:54 +0800

    npm i element-ui -S

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,13 +1,20 @@
 <template>
 	<div id="topbar">
-		我是Topbar
+		<div class="logo">
+			VueResumer
+		</div>
+		<div class="actions">
+			<button>注册</button>
+			<button>登录</button>
+		</div>
 	</div>
 </template>
 
 <style lang='scss'>
 	#topbar{
-		border: 1px solid red;
-		min-height: 100px;
+		display: flex;
+		justify-content: space-between;
+		padding: 16px;
 	}
 
 </style>
