463e60d840c8ee306a736b4afd1de920bdf86215
uchihasatoshi <uchihasatoshi@163.com>
2017-08-30 16:24:19 +0800

    优化tab切换代码

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,59 +3,34 @@
   <div id="editor">
     <nav>
       <ol>
-        <li v-bind:class="{active: currentTab===0}" v-on:click="currentTab=0">
+        <li v-for="i in [0,1,2,3,4,5]" v-bind:class="{active: currentTab===i}" v-on:click="currentTab=i">
           <svg class="icon">
-            <use xlink:href="#icon-gerenxinxi"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: currentTab===1}" v-on:click="currentTab=1">
-          <svg class="icon">
-            <use xlink:href="#icon-gongzuo"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: currentTab===2}" v-on:click="currentTab=2">
-          <svg class="icon">
-            <use xlink:href="#icon-shu_1"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: currentTab===3}" v-on:click="currentTab=3">
-          <svg class="icon">
-            <use xlink:href="#icon-huojiangmingdan"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: currentTab===4}" v-on:click="currentTab=4">
-          <svg class="icon">
-            <use xlink:href="#icon-daima"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: currentTab===5}" v-on:click="currentTab=5">
-          <svg class="icon">
-            <use xlink:href="#icon-dianhua-1"></use>
+            <use v-bind:xlink:href="`#icon-${icons[i]}`"></use>
           </svg>
         </li>
       </ol>
     </nav>
     <ol class="panes">
-    	<li v-bind:class="{active: currentTab===0}">1</li>
-    	<li v-bind:class="{active: currentTab===1}">2</li>
-    	<li v-bind:class="{active: currentTab===2}">3</li>
-    	<li v-bind:class="{active: currentTab===3}">4</li>
-    	<li v-bind:class="{active: currentTab===4}">5</li>
-    	<li v-bind:class="{active: currentTab===5}">6</li>
+      <li v-bind:class="{active: currentTab===0}">1</li>
+      <li v-bind:class="{active: currentTab===1}">2</li>
+      <li v-bind:class="{active: currentTab===2}">3</li>
+      <li v-bind:class="{active: currentTab===3}">4</li>
+      <li v-bind:class="{active: currentTab===4}">5</li>
+      <li v-bind:class="{active: currentTab===5}">6</li>
     </ol>
   </div>
 </template>
-
 <script>
-	export default{
-		data(){
-			return {
-				currentTab: 0
-			}
-		}
-	}
-</script>
+export default {
+  data() {
+    return {
+      currentTab: 0,
+      icons: ['gerenxinxi', 'gongzuo', 'shu_1', 'huojiangmingdan', 'daima', 'dianhua-1']
+    }
+  }
+}
 
+</script>
 <style lang="scss">
 #editor {
   min-height: 100px;
@@ -82,15 +57,15 @@
       }
     }
   }
-  .panes{
-  	border: 1px solid #000;
-  	>li{
-  		display: none;
-  	}
-  	>li.active{
-  		display: block;
-  		border: 1px solid red;
-  	}
+  .panes {
+    border: 1px solid #000;
+    >li {
+      display: none;
+    }
+    >li.active {
+      display: block;
+      border: 1px solid red;
+    }
   }
 }
 
