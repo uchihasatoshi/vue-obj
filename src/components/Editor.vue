67ba74ccc7d3c8f05c0f831e8807663a1692413f
uchihasatoshi <uchihasatoshi@163.com>
2017-08-30 16:02:34 +0800

    完成tab切换代码

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,41 +3,59 @@
   <div id="editor">
     <nav>
       <ol>
-        <li class="active">
+        <li v-bind:class="{active: currentTab===0}" v-on:click="currentTab=0">
           <svg class="icon">
             <use xlink:href="#icon-gerenxinxi"></use>
           </svg>
         </li>
-        <li>
+        <li v-bind:class="{active: currentTab===1}" v-on:click="currentTab=1">
           <svg class="icon">
             <use xlink:href="#icon-gongzuo"></use>
           </svg>
         </li>
-        <li>
+        <li v-bind:class="{active: currentTab===2}" v-on:click="currentTab=2">
           <svg class="icon">
             <use xlink:href="#icon-shu_1"></use>
           </svg>
         </li>
-        <li>
+        <li v-bind:class="{active: currentTab===3}" v-on:click="currentTab=3">
           <svg class="icon">
             <use xlink:href="#icon-huojiangmingdan"></use>
           </svg>
         </li>
-        <li>
+        <li v-bind:class="{active: currentTab===4}" v-on:click="currentTab=4">
           <svg class="icon">
             <use xlink:href="#icon-daima"></use>
           </svg>
         </li>
-        <li>
+        <li v-bind:class="{active: currentTab===5}" v-on:click="currentTab=5">
           <svg class="icon">
             <use xlink:href="#icon-dianhua-1"></use>
           </svg>
         </li>
       </ol>
     </nav>
-    <ol class="panes"></ol>
+    <ol class="panes">
+    	<li v-bind:class="{active: currentTab===0}">1</li>
+    	<li v-bind:class="{active: currentTab===1}">2</li>
+    	<li v-bind:class="{active: currentTab===2}">3</li>
+    	<li v-bind:class="{active: currentTab===3}">4</li>
+    	<li v-bind:class="{active: currentTab===4}">5</li>
+    	<li v-bind:class="{active: currentTab===5}">6</li>
+    </ol>
   </div>
 </template>
+
+<script>
+	export default{
+		data(){
+			return {
+				currentTab: 0
+			}
+		}
+	}
+</script>
+
 <style lang="scss">
 #editor {
   min-height: 100px;
@@ -47,8 +65,7 @@
     width: 80px;
 
     >ol>li {
-      border: 1px solid red;
-      padding: 8px 0;
+      padding: 16px 0;
       text-align: center;
 
       >.icon {
@@ -64,6 +81,16 @@
         }
       }
     }
+  }
+  .panes{
+  	border: 1px solid #000;
+  	>li{
+  		display: none;
+  	}
+  	>li.active{
+  		display: block;
+  		border: 1px solid red;
+  	}
   }
 }
 
